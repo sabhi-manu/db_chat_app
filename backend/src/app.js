2ef5f2d90632b8b7697444b0994ee386623fe92b
sabhi-manu <sabhimanu707@gmail.com>
2025-12-06 00:17:24 +0530

    cors update

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use(cors({
-  origin: "http://localhost:5173",
+ origin: ["http://localhost:5173", "https://talktogether-chat.netlify.app"],
   credentials: true
 }));
 
