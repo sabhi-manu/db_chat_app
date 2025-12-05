feaa3da5354c0eaa4d3c8bf9fe30333a8517a8f4
sabhi-manu <sabhimanu707@gmail.com>
2025-12-05 23:15:57 +0530

    port change

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const socketLogic = require('./src/config/socket')
 const server = http.createServer(app)
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL ,
     methods: ["GET", "POST"],
     credentials: true
   }
@@ -19,7 +19,7 @@ connectDB()
 
 socketLogic(io)
 
-
-server.listen(3000,()=>{
-    console.log(`server is runing on port http://localhost:3000`)
+const PORT = process.env.PORT || 5000;
+server.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
 })
