c9db6413f9f7481a8f832fc29f8c7e91c2a5a728
sabhi-manu <sabhimanu707@gmail.com>
2025-12-05 23:57:38 +0530

    update socket url

--- frontend/src/components/socket/socket.js
+++ frontend/src/components/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000",{withCredentials: true,})
+const socket = io("https://chat-backend-qfh7.onrender.com",{withCredentials: true,})
 
 export default socket
 
