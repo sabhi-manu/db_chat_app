83aaea8265610114eb3826d011afdd2e73164fe2
sabhi-manu <sabhimanu707@gmail.com>
2025-12-06 08:00:28 +0530

    base api update

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const connectDB = async()=>{
     try {
         await mongoose.connect(process.env.MONGO_URL)
-        console.log("mongoDB connect successfully.")
+        console.log("mongoDB connect successfully.")  
     } catch (error) {
         console.log("Error :",error.message)
         process.exit()
--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios  from "axios"
 
  const axiosInstance = axios.create({
-    baseURL:' http://localhost:3000/api',
+    baseURL:'https://chat-backend-qfh7.onrender.com/api',
     withCredentials: true
 })
 
